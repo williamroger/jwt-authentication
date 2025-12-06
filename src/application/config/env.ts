9e051088d659681f305c7ffada1bcbf35453bc05
William Roger <williamroger.frontend@gmail.com>
2025-12-06 01:19:43 -0300

    criado o caso de uso sign in para login do usuário

---
+++ src/application/config/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  databaseUrl: process.env.DATABASE_URL!,
+  jwtSecret: process.env.JWT_SECRET!,
+};
---
+++ src/application/useCases/SignInUseCase.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { prisma } from '../../lib/prisma';
+import { InvalidCredentials } from '../errors/InvalidCredentials';
+import { env } from '../../application/config/env';
+
+interface IInput {
+  email: string;
+  password: string;
+}
+
+interface IOutput {
+  accessToken: string;
+};
+
+export class SignInUseCase {
+  async execute({ email, password }: IInput): Promise<IOutput> {
+    const account = await prisma.account.findUnique({ where: { email } });
+
+    if (!account) {
+      throw new InvalidCredentials();
+    }
+
+    const isPasswordValid = await compare(password, account.password);
+
+    if (!isPasswordValid) {
+      throw new InvalidCredentials();
+    }
+
+    const accessToken = sign(
+      { sub: account.id },
+      env.jwtSecret,
+      { expiresIn: '1d' }
+    );
+
+    return { accessToken };
+  }
+}
